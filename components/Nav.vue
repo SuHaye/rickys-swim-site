484d264a6f23858303195fb04b4edea28cdf44b8
SuHaye <patricksuchy2@gmail.com>
2021-02-23 23:01:32 -0600

    Navbar layout redone

--- components/Nav.vue
+++ components/Nav.vue
@@ -10,7 +10,7 @@
                 <div class="flex">
                     <!-- Logo -->
                     <div 
-                        class="lg:absolute left-20 top-0 py-5 mx-auto"
+                        class="lg:absolute lg:left-10 xl:left-20 top-0 py-5 mx-auto"
                         >
                         <NuxtLink
                             to="/"
@@ -37,7 +37,9 @@
                     v-bind:class="{'hidden': !showMenu, 'flex': showMenu}">
 
                     <!-- Top Section -->
-                    <div class="flex flex-col lg:flex-row justify-end pl-10 lg:px-20">
+                    <div class="flex flex-col lg:flex-row justify-end pl-10 lg:px-10 xl:px-20">
+
+                        <div class="flex mb-5 lg:mb-2">
                         <!-- Sign In / Sign Up -->
                         <NuxtLink
                             class="hidden lg:block my-auto headtext font-semibold text-xl uppercase bg-gradient-to-l from-blue-200 to-white 
@@ -47,7 +49,6 @@
                             >Online Portal
                         </NuxtLink>
                         <!-- Social Media Links -->
-                        <div class="flex mb-5">
                             <NuxtLink
                                 to="/"
                                 class="text-4xl mr-3 lg:mx-3 hover:text-gray-300 w-max"
@@ -66,40 +67,40 @@
                         </div>
                     </div>
                     <!-- Bottom Section -->
-                    <div class="pl-10 lg:px-20 pb-10 lg:pb-0 pt-2 w-full">
-                        <div class="text-lg xl:text-xl 2xl:text-2xl font-semibold subtext tracking-widest uppercase lg:justify-end
+                    <div class="pl-10 lg:px-10 xl:px-20 pb-2 lg:pb-0 w-full">
+                        <div class="text-2xl lg:text-lg xl:text-xl 2xl:text-2xl font-semibold subtext tracking-widest uppercase lg:justify-end
                          flex flex-col lg:flex-row">
                             <NuxtLink
-                                class="hover:text-gray-300 hover:underline w-max"
+                                class="hover:text-gray-300 hover:underline pt-1 w-max lg:mx-2.5 my-1 lg:my-0"
                                 to="/"
                                 >Home
                             </NuxtLink>
-                            <span class="hidden lg:inline-block">|</span>
+                            <span class="spacer hidden lg:inline-block lg:mx-2.5">|</span>
                             <NuxtLink
-                                class="hover:text-gray-300 hover:underline w-max"
+                                class="hover:text-gray-300 hover:underline pt-1 w-max lg:mx-2.5 my-1 lg:my-0"
                                 to="/contact"
                                 >Events
                             </NuxtLink>
-                            <span class="hidden lg:inline-block">|</span>
+                            <span class="spacer hidden lg:inline-block lg:mx-2.5">|</span>
                             <NuxtLink
-                                class="hover:text-gray-300 hover:underline w-max"
+                                class="hover:text-gray-300 hover:underline pt-1 w-max lg:mx-2.5 my-1 lg:my-0"
                                 to="/events"
                                 >Contact
                             </NuxtLink>
-                            <span class="hidden lg:inline-block">|</span>
+                            <span class="spacer hidden lg:inline-block lg:mx-2.5">|</span>
                             <NuxtLink
-                                class="hover:text-gray-300 hover:underline w-max"
+                                class="hover:text-gray-300 hover:underline pt-1 w-max lg:mx-2.5 my-1 lg:my-0"
                                 to="/about"
                                 >About <span class="lg:hidden">Us</span>
                             </NuxtLink>
-                            <span class="hidden lg:inline-block">|</span>
+                            <span class="spacer hidden lg:inline-block lg:mx-2.5">|</span>
                             <NuxtLink
-                                class="hover:text-gray-300 hover:underline w-max"
+                                class="hover:text-gray-300 hover:underline pt-1 w-max lg:mx-2.5 my-1 lg:my-0"
                                 to="/safe"
                                 >Safe <span class="lg:hidden">Sport</span>
                             </NuxtLink>
                             <NuxtLink
-                                class="lg:hidden hover:text-gray-300 hover:underline w-max"
+                                class="lg:hidden hover:text-gray-300 hover:underline w-max my-1"
                                 to="/safe"
                                 >Online Portal
                             </NuxtLink>
@@ -136,4 +137,3 @@ export default {
 
 }
 </script>
-
